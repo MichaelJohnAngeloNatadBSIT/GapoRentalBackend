ec5944577eeb1f38406da1e7af66093026df35c5
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2022-04-26 10:20:03 +0800

    commit-04/26/22-10:19am

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,4 +12,50 @@ class ProductController extends Controller
 
         return response()->json($products);
     }
+
+    public function store(Request $request){
+        // Validation
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'category' => 'required',
+            'imageUrl' => 'required',
+        ]);
+
+        $product = Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'category' => $request->category,
+            'imageUrl' => $request->imageUrl,
+            'description' => $request->description,
+        ]);
+
+        return response()->json($product);
+
+    }
+
+    public function update(Request $request, Product $product){
+         // Validation
+         $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'category' => 'required',
+            'imageUrl' => 'required',
+        ]);
+
+        $product->update([
+            'name' => $request->name,
+            'price' => $request->price,
+            'category' => $request->category,
+            'imageUrl' => $request->imageUrl,
+            'description' => $request->description,
+        ]);
+
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return response()->json($product);    
+    }
 }
